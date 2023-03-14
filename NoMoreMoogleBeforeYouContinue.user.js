efcaa565735c41e03d48034dcc746bbe69bf6a9b
Davilarek <62715937+Davilarek@users.noreply.github.com>
2023-03-14 18:15:38 +0100

    Update NoMoreMoogleBeforeYouContinue.user.js

--- NoMoreMoogleBeforeYouContinue.user.js
+++ NoMoreMoogleBeforeYouContinue.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         No more google before you continue
-// @version      0.4
+// @version      0.5
 // @description  Universal solution to remove "Before you continue"
 // @author       Davilarek
 // @match        https://www.google.com
@@ -32,6 +32,7 @@
             var elements = Array.from(document.getElementsByTagName("form")).filter(x=>!checkStyleOrClass(x.parentElement, "display", "none"));
             elements[0].getElementsByTagName("button")[0].click();
             break;
+        case "https://www.google.com":
         case "https://google.com":
             // filter out body. In normal case there should be 3 DIV's. First is the search UI, second is "Before you continue dialog" and I don't know what is third.
             // this code filters out the search UI, so there is only the dialog and the thing I don't know what it is.
